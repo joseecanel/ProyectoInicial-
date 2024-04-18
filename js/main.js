7f27a3c4b242f8f393b30dcefd4ecaa9b0803650
joseecanel <joset08canelon@gmail.com>
2024-04-18 19:48:54 -0300

    anotaciones clase 2

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let perro = "Toby" // tipo de dato string 
+
+let numero = 30 // tipo de dato number 
+
+let verdadero = true // tipo de dato boolean 
+let falso = false // tipo de dato boolean 
+
+let indefinido; // tipo de dato undefined 
+
+let nulo = null // tipo de dato null
+
+const dni = 96015949
+
+let curso = "Desarrollo web 2"
+
+let year = 2024
+
+console.log(curso)
+
+console.log(year)
+
+let nombre = prompt ("Ingrese su nombre") 
+
+console.log(nombre)
+
+let numeroUno = 34;
+let numeroDos = 50;
+
+let resultado 
+
+resultado = numeroUno + numeroDos
+
+console.log(resultado)
+
+let nombreUno = "José"  
+let nombreDos = "Canelón"
+
+console.log(nombreUno + " " + nombreDos)
+
+let fullname = `Mi nombre es ${nombreUno} ${nombreDos}`
+
+console.log(fullname)
