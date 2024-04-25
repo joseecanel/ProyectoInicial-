117f83451fade96b431f18ff3d573856f9c3ee9a
joseecanel <joset08canelon@gmail.com>
2024-04-25 19:49:43 -0300

    actu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//clase 2
 let perro = "Toby" // tipo de dato string 
 
 let numero = 30 // tipo de dato number 
@@ -40,3 +41,48 @@ console.log(nombreUno + " " + nombreDos)
 let fullname = `Mi nombre es ${nombreUno} ${nombreDos}`
 
 console.log(fullname)
+
+//Clase 3
+
+let producto = "Heladera"
+let precio = 20
+let cantidad = prompt("Ingrese la cantidad")
+
+let total = precio * cantidad 
+
+alert(total)
+
+let num1 = 2
+let num2 = 2
+
+let result = num1 + num2 
+
+console.log(result)
+
+if(result > 3){
+    console.log("El numero es mayor que 3")
+}
+else{
+    console.log("No es mayor")
+}
+
+let lluvia = true
+let nube = true
+let pronostico = true 
+
+if (lluvia == false){
+    console.log("Esta lloviendo. Se recomienda usar paraguas")
+} else if (nube){
+    console.log("Est'a lloviendo y el cielo est'a nublado. Se recomienda usar paraguas")
+} else if (pronostico){
+    console.log("Est'a nublado. Se recomienda llevar paraguas")
+} else {console.log
+    console.log("No esta lloviendo")
+}
+
+if (!lluvia){
+    console.log("Esta lloviendo")
+}else{
+    console.log("No esta lloviendo")
+}
+
