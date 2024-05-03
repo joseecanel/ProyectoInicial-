fb8c1b9b685e680d40a179729d4f88dcb4654bb9
joseecanel <joset08canelon@gmail.com>
2024-05-02 21:57:36 -0300

    anotaciones clase 4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,58 @@
-let producto = "Manzana"
-let precio = 700
-let cantidad = prompt(`Ingrese la cantidad de ${producto}`)
+// function saludo1 (name) {
+//     console.log(`Hola ${name}`)
+// }
 
-let total = precio * cantidad 
+// saludo( "Ana" )
 
-alert(total)
+
+
+
+// function saludo2 (name) {
+//     let result = `Hola ${name}`
+//     return result 
+// }
+
+// console.log(saludo( "Ana" ))
+
+
+
+
+// const saludo = () => {
+//     console.log("hswihdeuhduhru")
+// }
+
+// saludo ()
+
+// //Hallar el area de un rectangulo
+
+// const areaderectangulo = ( base , altura ) =>{
+//     let result = base * altura
+//     return result 
+// }
+
+// console.log(areaderectangulo( 3, 5))
+
+// const parOimpar = ( num ) => {
+//     num = prompt("Ingrese un numero.....")
+//     if( num % 2 === 0 ){
+//         console.log(`el numero que ${num} es par`)
+//     }else{
+//         console.log(`el numero que ${num} no es par`)
+//     }
+// }
+
+// const totalDeVocales = ( vocales ) =>{
+//     vocales = prompt("Ingrese palabra")
+//     let result = vocales.match(/[aeiou]/gi).length
+//     return result
+// }
+
+// console.log(totalDeVocales())
+
+palabraReves ()
+
+function palabraReves ( word ) {
+    word = prompt("Ingrese palabra")
+    let result = word.split("").reverse().join("")
+    console.log(result)
+} 
